b296bb39e98c032654d3efb765c56c0f274da512
Marcus Feitoza <marcus.feitoza.filho@gmail.com>
2021-12-01 23:21:22 -0300

    feat(components): card-class add nav

--- src/components/card-class.js
+++ src/components/card-class.js
@@ -4,48 +4,55 @@ import {
   Heading,
   Text,
   Stack,
-} from "native-base"
+  Pressable,
+} from "native-base";
+import { useNavigation } from '@react-navigation/native';
 
 export default function CardClass({ name, location, startDate, endDate }) {
-  return (
-    <Box
-      w="100%"
-      rounded="lg"
-      overflow="hidden"
-      borderColor="coolGray.200"
-      borderWidth="1"
-      _dark={{
-        borderColor: "coolGray.600",
-        backgroundColor: "gray.700",
-      }}
-      _web={{
-        shadow: 2,
-        borderWidth: 0,
-      }}
-      _light={{
-        backgroundColor: "gray.50",
-      }}
-    >
-      <Stack p="4" space={3}>
 
-        <Stack space={2}>
-          <Heading size="md">
-            {name}
-          </Heading>
-          <Text
-            fontSize="sm"
-            _light={{
-              color: "primary.500",
-            }}
-            _dark={{
-              color: "primary.400",
-            }}
-            fontWeight="500"
-          >
-            {location}
-          </Text>
+  const navigation = useNavigation();
+
+  return (
+    <Pressable onPress={() => navigation.navigate('ClassHome')}>
+      <Box
+        w="100%"
+        rounded="lg"
+        overflow="hidden"
+        borderColor="coolGray.200"
+        borderWidth="1"
+        _dark={{
+          borderColor: "coolGray.600",
+          backgroundColor: "gray.700",
+        }}
+        _web={{
+          shadow: 2,
+          borderWidth: 0,
+        }}
+        _light={{
+          backgroundColor: "gray.50",
+        }}
+      >
+        <Stack p="4" space={3}>
+
+          <Stack space={2}>
+            <Heading size="md">
+              {name}
+            </Heading>
+            <Text
+              fontSize="sm"
+              _light={{
+                color: "primary.500",
+              }}
+              _dark={{
+                color: "primary.400",
+              }}
+              fontWeight="500"
+            >
+              {location}
+            </Text>
+          </Stack>
         </Stack>
-      </Stack>
-    </Box>
+      </Box>
+    </Pressable>
   )
 }
