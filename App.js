366eccabf99740e173e1bf76684162141b1e56d1
Marcus Feitoza <marcus.feitoza.filho@gmail.com>
2021-12-01 23:16:23 -0300

    feat(app)

--- App.js
+++ App.js
@@ -1,21 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import { NativeBaseProvider, Box } from 'native-base';
+
+import LoginScreen from './src/screens/login';
+import HomeScreen from './src/screens/home';
+import ClassHomeScreen from './src/screens/class-home';
+import ProfileScreen from './src/screens/profile';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+
+  const MyTheme = {
+    ...DefaultTheme,
+    colors: {
+      ...DefaultTheme.colors,
+      background: '#FFF',
+    },
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <NavigationContainer theme={MyTheme}>
+        <Stack.Navigator initialRouteName="Profile">
+          <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
+          <Stack.Screen name="Home" component={HomeScreen} options={{ title: "Cursos Ativos" }} />
+          <Stack.Screen name="ClassHome" component={ClassHomeScreen} options={{ title: "Curso" }} />
+          <Stack.Screen name="Profile" component={ProfileScreen} options={{ title: "Perfil" }} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
