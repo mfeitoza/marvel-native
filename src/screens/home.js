fe4bd0134adcf872458922ff8905a7bf6aaeec6e
Marcus Feitoza <marcus.feitoza.filho@gmail.com>
2021-12-08 00:09:08 -0300

    feat(screens): add home

--- src/screens/home.js
+++ src/screens/home.js
@@ -3,25 +3,30 @@ import React from 'react';
 import { Box, Stack } from 'native-base';
 
 import Layout from './layout'
-
 import CardClass from '../components/card-class';
-import Navigation from '../components/navigation'
+
+import { AuthContext } from '../contexts';
+
 
 export default function Home() {
+
+  const { state } = React.useContext(AuthContext);
+  const enrollments = state.user.enrollments;
+
+  console.log(enrollments[0])
+
   return (
     <Layout>
       <Stack space={4} direction="column">
-        <CardClass name="Lorem ipsum" location="Dollor stime dhudfa" />
-        <CardClass name="Lorem ipsum" location="Dollor stime dhudfa" />
-        <CardClass name="Lorem ipsum" location="Dollor stime dhudfa" />
-        <CardClass name="Lorem ipsum" location="Dollor stime dhudfa" />
-        <CardClass name="Lorem ipsum" location="Dollor stime dhudfa" />
-        <CardClass name="Lorem ipsum" location="Dollor stime dhudfa" />
-        <CardClass name="Lorem ipsum" location="Dollor stime dhudfa" />
-        <CardClass name="Lorem ipsum" location="Dollor stime dhudfa" />
-        <CardClass name="Lorem ipsum" location="Dollor stime dhudfa" />
-        <CardClass name="Lorem ipsum" location="Dollor stime dhudfa" />
-        <CardClass name="Lorem ipsum" location="Dollor stime dhudfa" />
+        {enrollments.map(enrollment => 
+          <CardClass 
+            key={enrollment.id}
+            id={enrollment.id}
+            name={enrollment.course.course.name}
+            location={enrollment.course.location} 
+          />
+          )
+        }
       </Stack>
     </Layout>
   )
