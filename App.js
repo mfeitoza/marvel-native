f7e8eb1c180d53086bb8279046823d4d3bfed93e
Marcus Feitoza <marcus.feitoza.filho@gmail.com>
2021-12-07 21:06:45 -0300

    feat(app): add auth logic

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import { NativeBaseProvider, Box } from 'native-base';
 
@@ -9,6 +10,8 @@ import LoginScreen from './src/screens/login';
 import HomeScreen from './src/screens/home';
 import ClassHomeScreen from './src/screens/class-home';
 import ProfileScreen from './src/screens/profile';
+
+import { AuthContext } from './src/contexts';
 
 const Stack = createNativeStackNavigator();
 
@@ -22,16 +25,80 @@ export default function App() {
     },
   };
 
+  const [authState, dispatch] = React.useReducer(
+    (prevState, action) => {
+      switch (action.type) {
+        case 'RESTORE_TOKEN':
+          return {
+            ...prevState,
+            userToken: action.token,
+            isLoading: false,
+          };
+        case 'SIGN_IN':
+          return {
+            ...prevState,
+            isSignout: false,
+            userToken: action.token,
+          };
+        case 'SIGN_OUT':
+          return {
+            ...prevState,
+            isSignout: true,
+            userToken: null,
+          };
+      }
+    },
+    {
+      isLoading: true,
+      isSignout: false,
+      userToken: null,
+    }
+  );
+
+  React.useEffect(() => {
+
+    const bootstrapAsync = async () => {
+      let userToken;
+
+      try {
+        userToken = await AsyncStorage.getItem('USER_TOKEN');
+      } catch (e) {
+        console.log(e);
+      }
+
+      dispatch({ type: 'RESTORE_TOKEN', token: userToken });
+    };
+
+    bootstrapAsync();
+  }, []);
+
+  const authContext = React.useMemo(
+    () => ({
+      signIn: async (token) => {
+        dispatch({ type: 'SIGN_IN', token });
+      },
+      signOut: () => dispatch({ type: 'SIGN_OUT' })
+    }),
+    []
+  );
+
   return (
-    <NativeBaseProvider>
-      <NavigationContainer theme={MyTheme}>
-        <Stack.Navigator initialRouteName="Profile">
-          <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
-          <Stack.Screen name="Home" component={HomeScreen} options={{ title: "Cursos Ativos" }} />
-          <Stack.Screen name="ClassHome" component={ClassHomeScreen} options={{ title: "Curso" }} />
-          <Stack.Screen name="Profile" component={ProfileScreen} options={{ title: "Perfil" }} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </NativeBaseProvider>
+    <AuthContext.Provider value={authContext}>
+      <NativeBaseProvider>
+        <NavigationContainer theme={MyTheme}>
+          <Stack.Navigator>
+            {authState.userToken == null ? (
+              <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
+            ) : (
+              <>
+                <Stack.Screen name="Home" component={HomeScreen} options={{ title: "Cursos Ativos" }} />
+                <Stack.Screen name="ClassHome" component={ClassHomeScreen} options={{ title: "Curso" }} />
+                <Stack.Screen name="Profile" component={ProfileScreen} options={{ title: "Perfil" }} />
+              </>
+            )}
+          </Stack.Navigator>
+        </NavigationContainer>
+      </NativeBaseProvider>
+    </AuthContext.Provider>
   );
 }
